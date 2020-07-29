ad0d35fe0ea2b6431b75b5187928c172caac7fff
Nguyen Anh Tuan <55679990+K18rs043@users.noreply.github.com>
2020-07-29 10:59:31 +0900

    add email and password

--- nodejs-workspace/practice/validate/user.validate.js
+++ nodejs-workspace/practice/validate/user.validate.js
@@ -3,6 +3,12 @@ module.exports.postId = function(req, res, next){
     if(!req.body.name){
         errors.push("name is required");
     }
+    if(!req.body.email){
+        errors.push("email is required");
+    }
+    if(!req.body.password){
+        errors.push("password is required");
+    }
     if(!req.body.phone){
         errors.push("Phone is required");
     }
